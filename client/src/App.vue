1c05bb0cd596375c4970841814849f39becb9e88
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-09-09 20:30:13 -0300

    iniciando projeto client-side

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
