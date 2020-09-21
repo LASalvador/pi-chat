758e440060a2719031b27c04bde9c22d29214a5c
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-09-20 22:45:39 -0300

    adicionando pagiina de chat

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Chat from '../views/Chat/Chat.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/chat',
+    name: 'chat',
+    component: Chat
   }
 ]
 
