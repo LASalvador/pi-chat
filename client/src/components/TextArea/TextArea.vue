616f43bb40d05891b12fbf452259d5d8f18a683f
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-09-23 19:40:19 -0300

    adicionando componente de TextArea

---
+++ client/src/components/TextArea/TextArea.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-textarea
+    no-resize
+    clearable
+    color="purplePi"
+    rows="1"
+    label="Digite uma mensagem"
+    :value="value"
+    @input="$emit('input', $event)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'TextArea',
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
