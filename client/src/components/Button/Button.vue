6db536db013a5a5468f4366620fa388246d5bb29
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-09-23 19:39:55 -0300

    adicionando component de botão

---
+++ client/src/components/Button/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-btn
+      dark
+      color="purplePi"
+    >
+      <slot></slot>
+    </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    icon: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
