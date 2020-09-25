df3ddac211a57bfda859b138b1ea8aac259709f1
alineelima <lima.alinee@gmail.com>
2020-09-25 00:10:45 +0000

    Ciração da pasta service, do controller e do service de conversa

---
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/ConversaController.java
@@ -0,0 +1,5 @@
+package br.com.fatec.springbootpi.controller;
+
+public class ConversaController {
+
+}
---
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/service/ConversaService.java
@@ -0,0 +1,5 @@
+package br.com.fatec.springbootpi.service;
+
+public class ConversaService {
+    
+}
