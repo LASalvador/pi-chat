b4be7d2c57204403602f67340e2acf9126257177
PauloHenrique7010 <paulohenrique7010@gmail.com>
2020-09-28 22:56:35 +0000

    Criando rota para buscar usuarios

--- spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/UsuarioController.java
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.fatec.springbootpi.entity.Usuario;
 import br.com.fatec.springbootpi.security.ServicoService;
+import br.com.fatec.springbootpi.service.UsuarioService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponses;
@@ -27,6 +28,9 @@ public class UsuarioController {
     @Autowired
     private ServicoService segService;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @PostMapping
 	@ApiOperation(value = "Inserir um novo usuário")
     public ResponseEntity<Usuario> cadastrarNovoUsuario(@RequestBody Usuario usuario, UriComponentsBuilder uriComponentsBuilder){
@@ -36,5 +40,12 @@ public class UsuarioController {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.setLocation(uriComponentsBuilder.path("/usuario/" + usuario.getIdUsuario()).build().toUri());
         return new ResponseEntity<Usuario>(usuario, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value="/{id}")
+    @ApiOperation(value = "Buscar todos usuários")
+    public Usuario buscarUsuarioPorID(@PathVariable("id") Long Id){
+        return usuarioService.buscarPorId(Id);
+        
     }
 }
