23263fdbb80a964a74044a08c4400d18fa302645
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-09-30 11:14:21 -0300

    adicionando componente de noteCard

---
+++ client/src/components/NoteCard/NoteCard.vue
@@ -0,0 +1,28 @@
+<template>
+   <v-card
+        :color="note.style.bg"
+        :dark="note.style.darken"
+        height="100%"
+    >
+        <v-card-title class="headline">{{note.title}}</v-card-title>
+
+        <v-card-text v-html="note.text.replace('\n','</br>')">
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'NoteCard',
+  props: {
+    note: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
