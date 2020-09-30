820444dd06657859252619605d3a058a26a4e99a
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-09-30 11:13:57 -0300

    adicionando componente de input

---
+++ client/src/components/Input/Input.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-text-field
+    color="bluePi"
+    :label="label" 
+    :value="value"
+    @input="$emit('input', $event)"
+    required
+  />
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
