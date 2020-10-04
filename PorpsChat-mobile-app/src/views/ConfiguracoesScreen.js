03486a84436f23d2388e3389ed003160e8500344
Monica261 <desenvolvimento.midiasim@gmail.com>
2020-10-04 16:46:32 -0300

    adicionando links na tela de Configurações

--- PorpsChat-mobile-app/src/views/ConfiguracoesScreen.js
+++ PorpsChat-mobile-app/src/views/ConfiguracoesScreen.js
@@ -4,20 +4,27 @@ import { SectionList, StyleSheet, Text, View } from 'react-native';
 
 const Configuracoes = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
+    <View style={styles.containerText}>
+      <Text style={styles.texttop}>Chat Porps</Text>
+      <Text style={styles.textconf}>Configurações</Text>
+      <Text style={styles.text}
+      onPress={() => Linking.openURL('#')}>
         ⚙ CONFIGURAÇÃO 1
       </Text>
-      <Text>
+      <Text style={styles.text} 
+      onPress={() => Linking.openURL('#')}>
         ⚙ CONFIGURAÇÃO 2
       </Text>
-      <Text>
+      <Text style={styles.text} 
+      onPress={() => Linking.openURL('#')}>
         ⚙ CONFIGURAÇÃO 3
       </Text>
-      <Text>
+      <Text style={styles.text} 
+      onPress={() => Linking.openURL('#')}>
         ⚙ CONFIGURAÇÃO 4
       </Text>
-      <Text>
+       <Text style={styles.text} 
+      onPress={() => Linking.openURL('#')}>
         ⚙ CONFIGURAÇÃO 5
       </Text>
     </View>
@@ -25,10 +32,23 @@ const Configuracoes = () => {
 }
 
 const styles = StyleSheet.create({
- container:{
-   margin: 50,
+ containerText:{
+   margin: 30,
    textAlign: 'center',
+ }, 
+ text:{
+   padding: 10,
  },
+ texttop:{
+   textAlign: 'left',
+   fontWeight: 'bold',
+   fontSize: 25,
+ },
+ textconf:{
+   fontSize:19,
+   textAlign: 'left',
+   marginBottom:10,
+ }
 });
 
 export default Configuracoes;
