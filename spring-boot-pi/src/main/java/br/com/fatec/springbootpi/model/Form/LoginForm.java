fb9344290e853337ae71503858aeb08bd7f5f772
alineelima <lima.alinee@gmail.com>
2020-10-15 22:45:55 +0000

    Form de login, autenticacao com spring e gerar token

---
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/model/Form/LoginForm.java
@@ -0,0 +1,45 @@
+package br.com.fatec.springbootpi.model.Form;
+
+public class LoginForm {
+
+    private String document;
+
+    private String password;
+
+    private String autorizacao;
+
+    private String token;
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getAutorizacao() {
+        return autorizacao;
+    }
+
+    public void setAutorizacao(String autorizacao) {
+        this.autorizacao = autorizacao;
+    }
+    
+}
--- spring-boot-pi/src/main/java/br/com/fatec/springbootpi/security/SecurityConfig.java
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.fatec.springbootpi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -34,5 +35,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean //disponibilza para que possa usar em outros lugares com @autowired
     public PasswordEncoder passwordEncoderBean(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
