3c8c23bbf8fbf315597a6865a06d8daf20bd4567
Gamebielo <gabrielcarvalho20@gmail.com>
2020-10-23 22:38:51 +0000

    New route /atividade/idUser (GET)

--- spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/AtividadeController.java
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/AtividadeController.java
@@ -1,5 +1,7 @@
 package br.com.fatec.springbootpi.controller;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonView;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import br.com.fatec.springbootpi.entity.Atividade;
 import br.com.fatec.springbootpi.model.AtividadeForm;
+import br.com.fatec.springbootpi.repository.AtividadeRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import br.com.fatec.springbootpi.service.AtividadeService;
@@ -21,6 +24,9 @@ public class AtividadeController {
     @Autowired
     private AtividadeService atividadeService;
 
+    @Autowired
+    private AtividadeRepository atividadeRepository;
+
     @JsonView(View.AtividadeResumo.class)
     @PostMapping
     @ApiOperation(value = "Inserir uma nova atividade")
@@ -31,5 +37,12 @@ public class AtividadeController {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.setLocation(uriComponentsBuilder.path("/atividade/" + atividade.getIdAtividade()).build().toUri());
         return new ResponseEntity<Atividade>(atividade, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @JsonView(View.AtividadeResumo.class)
+    @GetMapping(value = "{id}")
+    @ApiOperation(value = "Buscar atividades por ID do usuário")
+    public List<Atividade> pegarMensagensPorConversa(@PathVariable("id") Long id) {
+        return atividadeRepository.getAtividadesPorUsuario(id);
     }
 }
