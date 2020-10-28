26bfeed34c08ae0dca20405f396133138b5379d7
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-10-28 11:22:18 -0300

    adicionando tratamento para erros do axios

--- client/src/services/http.js
+++ client/src/services/http.js
@@ -1,7 +1,45 @@
 import axios from 'axios'
+import store from '../store/index'
 
 const api = axios.create({
-  baseURL: 'https://8080-fea942d2-c440-4a50-b54e-e8db52ba1caf.ws-us02.gitpod.io/spring-app/'
+  baseURL: 'https://8080-cb3ebcac-3f98-4b5f-a7a4-96c75698d793.ws-us02.gitpod.io/spring-app/'
 })
+
+function _beforeRequest (request) {
+  const token = store.getters.getToken
+  if (token) {
+    request.headers.Authorization = `Bearer ${token}`
+  }
+  store.commit('beforeRequest')
+  return request
+}
+
+function _afterRequest (response) {
+  store.commit('afterRequest', {
+    mensagem: 'EBA!! Sucesso na requisição!!',
+    cor: '#43A047'
+  })
+  return response
+}
+
+function _onError (error) {
+  let mensagem
+  if (error.message.includes('401')) {
+    mensagem = 'OPS! Você não está autenticado'
+  } else if (error.message.includes('403')) {
+    mensagem = 'OPS! Você não tem permissão para esta ação!!'
+  } else {
+    mensagem = 'OPS! Erro ao realizar requisição'
+  }
+
+  store.commit('afterRequest', {
+    mensagem: mensagem,
+    cor: '#E53935'
+  })
+  return Promise.reject(error)
+}
+
+api.interceptors.request.use(_beforeRequest, _onError)
+api.interceptors.response.use(_afterRequest, _onError)
 
 export default api
