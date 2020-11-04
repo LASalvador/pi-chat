45cbc07c029b0239d319a2f1d7b2b9f193ab3a82
LASalvador <lucas.salvador11358@gmail.com>
2020-11-04 14:59:27 +0000

    adicionando rota para listar os tipos de usuarrio

--- spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/TipoUsuarioController.java
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/TipoUsuarioController.java
@@ -1,10 +1,15 @@
 package br.com.fatec.springbootpi.controller;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +38,13 @@ public class TipoUsuarioController {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.setLocation(uriComponentsBuilder.path("/tipoUsuario/" + tipoUsuario.getIdTipoUsuario()).build().toUri());
         return new ResponseEntity<TipoUsuario>(tipoUsuario, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @JsonView(View.TipoUsuarioResumo.class)
+    @GetMapping
+    @ApiOperation(value = "Lista todos os usuarios")
+    public List<TipoUsuario> buscarUsuarios() {
+        return tipoUsuarioService.buscarTiposUsuarios();
+
     }
 }
--- spring-boot-pi/src/main/java/br/com/fatec/springbootpi/service/TipoUsuarioService.java
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/service/TipoUsuarioService.java
@@ -1,5 +1,7 @@
 package br.com.fatec.springbootpi.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,10 @@ public class TipoUsuarioService {
         }
     
         return tipoUsuario;
+    }
+
+    public List<TipoUsuario> buscarTiposUsuarios() {
+        return tipoUserRepo.findAll();
     }
 
 }
