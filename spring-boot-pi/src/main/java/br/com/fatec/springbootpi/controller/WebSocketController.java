1090b91651ec4940ec04c164d559288e4d80d436
LASalvador <lucas.salvador11358@gmail.com>
2020-11-11 01:11:05 +0000

    adicionando cors no websocket

--- spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/WebSocketController.java
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/WebSocketController.java
@@ -9,8 +9,10 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin
 public class WebSocketController {
 
     @Autowired
