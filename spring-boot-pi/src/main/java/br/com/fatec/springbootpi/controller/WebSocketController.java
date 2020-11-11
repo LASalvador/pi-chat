02ea857131370806b26fa9786a0f0adf79670333
lrsonnewend <lucas.lr184@gmail.com>
2020-11-11 23:31:15 +0000

    configurando websocket -> backend

--- spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/WebSocketController.java
+++ spring-boot-pi/src/main/java/br/com/fatec/springbootpi/controller/WebSocketController.java
@@ -1,54 +1,32 @@
 package br.com.fatec.springbootpi.controller;
 
-import java.time.LocalDateTime;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.fatec.springbootpi.model.MensagemForm;
+import br.com.fatec.springbootpi.service.MensagemService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
-@Controller
-@CrossOrigin
+@RestController
+@Api(value = "WebSocket")
 public class WebSocketController {
 
     @Autowired
-    SimpMessagingTemplate simpMessagingTemplate;
-    String destination = "/topic/messages";
+    private SimpMessagingTemplate simpMessagingTemplate;
 
-    ExecutorService executorService = Executors.newFixedThreadPool(1);
-    Future<?> submittedTask;
+    @Autowired
+    private MensagemService msgService;
 
-    @MessageMapping("/start")
-    public void startTask() {
-        if (submittedTask != null) {
-            simpMessagingTemplate.convertAndSend(destination, "Task already started");
-            return;
-        }
-        simpMessagingTemplate.convertAndSend(destination, "Started task");
-        submittedTask = executorService.submit(() -> {
-            while (true) {
-                simpMessagingTemplate.convertAndSend(destination, LocalDateTime.now().toString() + ": doing some work");
-                Thread.sleep(10000);
-            }
-        });
-    }
-
-    @MessageMapping("/stop")
-    @SendTo("/topic/messages")
-    public String stopTask() {
-        if (submittedTask == null) {
-            return "Task not running";
-        }
-        try {
-            submittedTask.cancel(true);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return "Error occurred while stopping task due to: " + ex.getMessage();
-        }
-        return "Stopped task";
+    @ApiOperation(value = "Rota para chat flutuante utilizando websocket")
+    @MessageMapping("/chat")
+    public void sendMessage(@RequestBody MensagemForm novaMensagem) {
+        System.out.println("Mensagem enviada: " + novaMensagem.getConteudoMsg() + " para: " + novaMensagem.getIdUsuario());
+        
+        msgService.criarMensagem(novaMensagem.getConteudoMsg(),novaMensagem.getIdUsuario(), novaMensagem.getIdConversa());
+        
+        simpMessagingTemplate.convertAndSend("/topic/messages/" + novaMensagem.getIdConversa(), novaMensagem);
     }
 }
