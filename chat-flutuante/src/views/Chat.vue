7b48e57af8d73407a0e4b6ae72b984f4d292062c
Mayara Brigida <mayara.brigidam@gmail.com>
2020-11-18 10:13:16 -0300

    criação de chat flutuante

---
+++ chat-flutuante/src/views/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-card>
+    <v-toolbar dense color="#005796">
+      <v-toolbar-title class="white--text">PorpsChat</v-toolbar-title>
+    </v-toolbar>
+    <v-card height="380" flat>
+    </v-card>
+      <div class="d-flex flex-column justify-center ml-1">
+        <div class="d-flex flex-row">
+          <v-text-field
+            label="Mensagem"
+            outlined
+          ></v-text-field>
+          <v-btn icon>
+            <v-icon>mdi-send</v-icon>
+          </v-btn>
+        </div>
+      </div>
+    </v-card>
+  </v-app>
+</template>
