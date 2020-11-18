46d612cfc49827353356325b76e30b85a9be8ab7
Mayara Brigida <mayara.brigidam@gmail.com>
2020-11-18 10:14:33 -0300

    Habilitação do vue roter

--- chat-flutuante/src/views/Home.vue
+++ chat-flutuante/src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <iframe class="chat" scrolling="no" src="/chat" v-if="active"/>
+      <v-btn
+        class="mx-2"
+        fab
+        right
+        bottom
+        fixed
+        dark
+        color="primary"
+        @click="active = !active"
+      >
+        <v-icon dark>
+          mdi-forum
+        </v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      active: false
+    }
   }
 }
 </script>
+
+<style scoped>
+.chat{
+  width: 350px;
+  height: 500px;
+  position:fixed;
+  right: 20px;
+  bottom: 90px;
+  border-radius: 15px;
+  box-shadow: 5px 10px #888888;
+}
+</style>
