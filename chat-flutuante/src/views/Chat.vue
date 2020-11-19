ec947d9f7950833d5211fbcc6699c390623fe350
Mayara Brigida <mayara.brigidam@gmail.com>
2020-11-19 07:42:43 -0300

    Colocando interação no chat fluatuante

--- chat-flutuante/src/views/Chat.vue
+++ chat-flutuante/src/views/Chat.vue
@@ -5,15 +5,15 @@
       <v-toolbar-title class="white--text">PorpsChat</v-toolbar-title>
     </v-toolbar>
     <v-card height="380" flat class="overflow-y-auto overflow-x-hidden">
-      <div class="d-flex flex-row">
+      <div class="d-flex flex-row ma-2">
         <v-icon large color="red lighten-1">mdi-comment-arrow-right</v-icon>
-        <v-card color="#e4e4e4" class="ma-1" rounded>
+        <v-card color="#e4e4e4" rounded>
           <span class="pa-1 ma-1">Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</span>
         </v-card>
       </div>
-      <div class="d-flex flex-row">
-        <v-card color="#e4e4e4" class="ma-1" rounded>
-          <span class="pa-1 ma-1">Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</span>
+      <div class="d-flex flex-row ma-2 justify-end" v-for="(msg, index) in conversa" :key="index">
+        <v-card color="#e4e4e4" rounded>
+          <span class="pa-1 ma-1">{{ msg.conteudo }}</span>
         </v-card>
         <v-icon large color="green lighten-1">mdi-comment-quote</v-icon>
       </div>
@@ -23,8 +23,9 @@
           <v-text-field
             label="Mensagem"
             outlined
+            v-model="conteudo"
           ></v-text-field>
-          <v-btn icon>
+          <v-btn icon @click="enviarMensagem">
             <v-icon>mdi-send</v-icon>
           </v-btn>
         </div>
@@ -32,5 +33,26 @@
     </v-card>
   </v-app>
 </template>
+<script>
+
+export default {
+  name: 'Chat',
+  components: {
+  },
+  data () {
+    return {
+      conversa: [],
+      conteudo: ''
+    }
+  },
+  methods: {
+    enviarMensagem () {
+      this.conversa.push({ conteudo:this.conteudo })
+      this.conteudo=''
+    }
+  }
+}
+</script>
+
 <style scoped>
 </style>
