51cf59a1e9225c3e6ccebaad83fc2f955f8d4eef
gamebielo <gabrielcarvalho20@gmail.com>
2020-11-23 21:20:33 -0300

    Initial files for IA

---
+++ IA/data.py
@@ -0,0 +1,15 @@
+RESPONSES = [
+    {
+        'tags': ['oi', 'olá'],
+        'responses': ['Olá, como posso ajudá-lo?', 'Oi, tudo bem com você? como posso ser útil?']
+    },
+    {
+        'tags': ['vocês fazem', 'trabalham', 'fazem', 'trabalho de vocês'],
+        'responses': ['A Anova é uma empresa que fornece sistemas inteligentes para diversas áreas no mercado. \
+                      O sistema e-condutor é uma plataforma online de gestão para autoescolas.']
+    },
+    {
+        'tags': ['horário', 'funcionamento', 'Qual horario'],
+        'responses': ['O horário de funcionamento é de segunda a sexta, das 08:00h às 22:00h']
+    }
+]
