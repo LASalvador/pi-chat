ba0438ade95832ea4262b45985a969ac11635d53
lucas <lucas.lr184@gmail.com>
2020-11-23 21:35:31 -0300

    palavras-chave para chatbot

--- IA/data.py
+++ IA/data.py
@@ -1,7 +1,11 @@
 RESPONSES = [
     {
-        'tags': ['oi', 'olá'],
-        'responses': ['Olá, como posso ajudá-lo?', 'Oi, tudo bem com você? como posso ser útil?']
+        'tags': ['oi', 'olá', 'salve'],
+        'responses': ['Olá, como posso ajudá-lo?', 'Salve, posso ajudar?','Oi, tudo bem com você? como posso ser útil?']
+    },
+    {
+        'tags': ['ok', 'obrigado', 'tchau', 'até mais'],
+        'responses': ['Até mais! Se precisar, estou aqui!', 'Sempre estarei disponível :)']
     },
     {
         'tags': ['vocês fazem', 'trabalham', 'fazem', 'trabalho de vocês'],
@@ -9,7 +13,11 @@ RESPONSES = [
                       O sistema e-condutor é uma plataforma online de gestão para autoescolas.']
     },
     {
-        'tags': ['horário', 'funcionamento', 'Qual horario'],
+        'tags': ['documentos', 'armazenamento', 'onde é feito o armazenamento'],
+        'responses': ['Os documentos essenciais ficam salvos no nosso', 'Você pode acessar seus documentos acessando o menu lateral.']
+    },
+    {
+        'tags': ['horário', 'funcionamento', 'Qual horário', 'Qual o horário'],
         'responses': ['O horário de funcionamento é de segunda a sexta, das 08:00h às 22:00h']
     }
 ]
